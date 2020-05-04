6c6f4884e2d52870a9a8dc80faa993ef8875395a
Eriz <eriz@zaramateo.com>
2020-05-04 17:34:42 +0200

    Some changes to the init file to make it work

--- darwinexapis/__init__.py
+++ darwinexapis/__init__.py
@@ -1,18 +1,18 @@
 # APIs:
-from darwinexapis.API.InfoAPI.DWX_Info_API import DWX_Info_API
+from .API.InfoAPI.DWX_Info_API import DWX_Info_API
 
-from darwinexapis.API.InvestorAccountInfoAPI.DWX_AccInfo_API import DWX_AccInfo_API
+from .API.InvestorAccountInfoAPI.DWX_AccInfo_API import DWX_AccInfo_API
 
-from darwinexapis.API.QuotesAPI.DWX_Quotes_API import DWX_Quotes_API
+from .API.QuotesAPI.DWX_Quotes_API import DWX_Quotes_API
 
-from darwinexapis.API.TradingAPI.DWX_Trading_API import DWX_Trading_API
+from .API.TradingAPI.DWX_Trading_API import DWX_Trading_API
 
-from darwinexapis.API.WebSocketAPI.DWX_WebSocket_API import DWX_WebSocket_API
+from .API.WebSocketAPI.DWX_WebSocket_API import DWX_WebSocket_API
 
-from darwinexapis.API.TickDataAPI.DWX_TickData_Downloader_API import DWX_TickData_Downloader_API
-from darwinexapis.API.TickDataAPI.DWX_TickData_Reader_API import DWX_TickData_Reader_API
+from .API.TickDataAPI.DWX_TickData_Downloader_API import DWX_TickData_Downloader_API
+from .API.TickDataAPI.DWX_TickData_Reader_API import DWX_TickData_Reader_API
 
-from darwinexapis.API.DarwinDataAnalyticsAPI.DWX_Data_Analytics_API import DWX_Darwin_Data_Analytics_API
+from .API.DarwinDataAnalyticsAPI.DWX_Data_Analytics_API import DWX_Darwin_Data_Analytics_API
 
 # Minions:
-from darwinexapis.MINIONS.dwx_graphics_helpers import DWX_Graphics_Helpers
+from .MINIONS.dwx_graphics_helpers import DWX_Graphics_Helpers
